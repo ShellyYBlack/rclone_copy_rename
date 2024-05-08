dc3ee30178e32d3926b2bf3ec8fa76a03db853af
ShellyYBlack <74626165+ShellyYBlack@users.noreply.github.com>
2024-05-08 16:24:02 -0400

    Add files via upload

---
+++ rclone_copy_rename_dupes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# List all files and IDs, and save as TXT
+# rclone lsf -R --files-only --format pi GDrive:Born\ Digital/Born\ Digital\ Initiative\ 1.0/Electronic\ File\ Transfers/Shelly_Test > $HOME/Desktop/path_ID_list.txt
+
+while IFS=';' read -r path ID
+do
+    if ! [ -f $HOME/Desktop/rclone/$path ]; then
+        rclone backend copyid -Pv --log-file $HOME/Desktop/log.txt GDrive: $ID $HOME/Desktop/rclone/$path
+        i=1
+    elif [ -f $HOME/Desktop/rclone/$path ]; then
+        rclone backend copyid -Pv --log-file $HOME/Desktop/log.txt GDrive: $ID $HOME/Desktop/rclone/"${path%.*}"_copy""$i""."${path##*.}"
+    fi
+    (( ++i ))
+done < $HOME/Desktop/path_ID_list.txt
